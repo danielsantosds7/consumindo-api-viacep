288fda0edfb016ecb4864be5ddf3788c094faace
Daniel <daniel@MacBook-Pro-de-Daniel.local>
2023-05-02 20:40:58 -0300

    minimal api - consumindo uma api de consulta ao cep

---
+++ MinhaApi/ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Refit;
+
+namespace MinhaApi
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);        
+    }
+}
---
+++ MinhaApi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Refit;
+
+namespace MinhaApi
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.Write("Informe o CEP: ");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informacoes do CEP: " + cepInformado);
+
+                var address = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.Write($"\nLogradouro: {address.Logradouro}\nBairro: {address.Bairro}\nCidade: {address.Localidade}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta do CEP: " + e.Message);
+            }
+        }
+    }
+}
